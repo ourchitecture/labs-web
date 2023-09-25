3cab1a7c2ab9418a1c1b5523dab9c9aec1774ecd
Ourchitecture <97544811+ourchitectureio@users.noreply.github.com>
2023-09-25 16:02:39 -0400

    chore: clean up
    Signed-off-by: Ourchitecture <97544811+ourchitectureio@users.noreply.github.com>

--- src/experiments/todo/implementations/html-and-expressjs/tests/todos.spec.js
+++ src/experiments/todo/implementations/html-and-expressjs/tests/todos.spec.js
@@ -316,13 +316,3 @@ test.describe('Mark all as complete', () => {
         ).toBe(true)
     })
 })
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
